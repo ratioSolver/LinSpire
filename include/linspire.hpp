4f797a7167071216585b7d3a6e28326c66cb1a79
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-09-15 15:26:06 +0200

    Implement initial classes for variable and solver in linspire namespace

--- include/linspire.hpp
+++ include/linspire.hpp
@@ -1,1 +1,22 @@
 #pragma once
+#include "lin.hpp"
+#include "inf_rational.hpp"
+#include <vector>
+
+namespace linspire
+{
+  class var
+  {
+  public:
+    var(const utils::rational &lb = utils::rational::negative_infinite, const utils::rational &ub = utils::rational::positive_infinite) : lb(lb), ub(ub) {}
+
+  private:
+    utils::inf_rational lb, ub, val; // lower bound, upper bound, value
+  };
+
+  class solver
+  {
+  private:
+    std::vector<var> variables;
+  };
+} // namespace linspire
