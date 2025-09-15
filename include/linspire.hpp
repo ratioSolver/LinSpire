b52d058158494187b7e8b670eb454e575e74bb15
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-09-15 17:02:10 +0200

    Refactor linspire library to use json submodule and implement variable serialization to JSON

--- include/linspire.hpp
+++ include/linspire.hpp
@@ -1,22 +1,33 @@
 #pragma once
 #include "lin.hpp"
 #include "inf_rational.hpp"
+#include "json.hpp"
 #include <vector>
 
 namespace linspire
 {
-  class var
-  {
-  public:
-    var(const utils::rational &lb = utils::rational::negative_infinite, const utils::rational &ub = utils::rational::positive_infinite) : lb(lb), ub(ub) {}
-
-  private:
-    utils::inf_rational lb, ub, val; // lower bound, upper bound, value
-  };
-
   class solver
   {
+  public:
+    [[nodiscard]] utils::var new_var(const utils::rational &lb = utils::rational::negative_infinite, const utils::rational &ub = utils::rational::positive_infinite) noexcept;
+
+    void new_lt(const utils::lin &lhs, const utils::lin &rhs, bool strict = false);
+
   private:
-    std::vector<var> variables;
+    class var
+    {
+    public:
+      var(const utils::rational &lb = utils::rational::negative_infinite, const utils::rational &ub = utils::rational::positive_infinite) : lb(lb), ub(ub) {}
+
+      [[nodiscard]] json::json to_json() const noexcept;
+
+    private:
+      utils::inf_rational lb, ub, val; // lower bound, upper bound, value
+    };
+
+    std::vector<var> variables;               // index is the variable id
+    std::map<utils::var, utils::lin> tableau; // variable -> expression
   };
+
+  json::json to_json(const utils::inf_rational &r) noexcept;
 } // namespace linspire
--- src/linspire.cpp
+++ src/linspire.cpp
@@ -1,1 +1,35 @@
 #include "linspire.hpp"
+
+namespace linspire
+{
+    utils::var solver::new_var(const utils::rational &lb, const utils::rational &ub) noexcept
+    {
+        variables.emplace_back(lb, ub);
+        return variables.size() - 1;
+    }
+
+    json::json solver::var::to_json() const noexcept
+    {
+        json::json j = linspire::to_json(val);
+        if (!is_infinite(lb))
+            j["lb"] = linspire::to_json(lb);
+        if (!is_infinite(ub))
+            j["ub"] = linspire::to_json(ub);
+        return j;
+    }
+
+    json::json to_json(const utils::inf_rational &r) noexcept
+    {
+        json::json j;
+        j["num"] = r.get_rational().numerator();
+        j["den"] = r.get_rational().denominator();
+        if (!is_zero(r.get_infinitesimal()))
+        {
+            json::json jinf;
+            jinf["num"] = r.get_infinitesimal().numerator();
+            jinf["den"] = r.get_infinitesimal().denominator();
+            j["inf"] = jinf;
+        }
+        return j;
+    }
+} // namespace linspire
